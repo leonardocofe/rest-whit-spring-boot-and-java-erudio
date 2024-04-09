23f6f713e2ccc6613569554fb1f4d7d31f9a9999
Leonardo Correa <leonardocofe04@gmai.com>
2024-04-09 19:05:22 -0300

    First Steps in Java whit Spring Boot

---
+++ demo/src/main/java/br/com/nerna/CursoApiApplication.java
@@ -0,0 +1,15 @@
+package br.com.nerna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class CursoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApiApplication.class, args);
+	}
+
+}
